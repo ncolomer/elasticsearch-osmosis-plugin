57592ed1c3d703e7fbf035f058353696da3a5212
Nicolas Colomer <ncolomer@octo.com>
2013-02-19 21:21:47 +0100

    Update Osmosis version from 0.41 to 0.42

--- lib/install.sh
+++ lib/install.sh
@@ -2,16 +2,17 @@
 
 DIR=`pwd`/`dirname $0`
 OUTPUT=$DIR/output.log
+OSMOSIS_VERSION=0.42
 
 cd $DIR
 
 # Install osmosis-core
 echo "Installing osmosis-core library into the local Maven repository"
 mvn install:install-file \
-	-Dfile=osmosis-core-0.41.jar \
+	-Dfile=osmosis-core-$OSMOSIS_VERSION.jar \
 	-DgroupId=org.openstreetmap \
 	-DartifactId=osmosis-core \
-	-Dversion=0.41 \
+	-Dversion=$OSMOSIS_VERSION \
 	-Dpackaging=jar >> $OUTPUT
 if [ $? -ne 0 ]; then
 	echo "Failed: see $OUTPUT logfile for details"
@@ -21,10 +22,10 @@ fi
 # Install osmosis-xml
 echo "Installing osmosis-xml library into the local Maven repository"
 mvn install:install-file \
-	-Dfile=osmosis-xml-0.41.jar \
+	-Dfile=osmosis-xml-$OSMOSIS_VERSION.jar \
 	-DgroupId=org.openstreetmap \
 	-DartifactId=osmosis-xml \
-	-Dversion=0.41 \
+	-Dversion=$OSMOSIS_VERSION \
 	-Dpackaging=jar >> $OUTPUT
 if [ $? -ne 0 ]; then
 	echo "Failed: see $OUTPUT logfile for details"
